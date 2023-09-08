036e1ac6b9dcbf40a2c1635651bb1a0add3be7f3
TalionSlade <arpan01ghosh1307@gmail.com>
2023-09-08 11:13:15 +0530

    initial commit

---
+++ src/main/java/com/wellsfargo/training/ezloans/EzloansApplication.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.training.ezloans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzloansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzloansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wellsfargo/training/ezloans/EzloansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.training.ezloans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzloansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
