2a0febe35d0a6250f87624a63a5ee8cb762d4611
vardhahanees <aneesmoidu_b190752ec@nitc.ac.in>
2023-09-18 21:22:51 +0530

    Login page

--- Ezloans-react/src/App.js
+++ Ezloans-react/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import NavBar from './components/Navbar';
+// import NavBar from './components/Navbar';
 import Registeration from './components/Registeration';
 import Login from './components/Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -10,20 +10,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Loan Management System</h1>
+      {/* <header className="App-header">
+        <h1>EZLOANS</h1>
 
-      </header>
+      </header> */}
       <section>
         <div style={{
-          backgroundImage:"url(/images/R.jpg)",
           backgroundSize:"cover",
           backgroundRepeat:"no-repeat",
           minHeight:"100vh",
           minWidth:"100vh"
         }}>
         <Router>
-          <NavBar/>
+          {/* <NavBar/> */}
           <Routes>
             <Route path="/register" element={<Registeration/>}/>
             <Route path="/login" element={<Login/>}/>
@@ -33,7 +32,7 @@ function App() {
       </section>
       <footer className="footer">
         <p>
-          &copy; All Rights Reserved to Wells Fargo
+          &copy; All Rights Reserved to Ezloans
         </p>
 
       </footer>
--- Ezloans-react/src/components/Login.js
+++ Ezloans-react/src/components/Login.js
@@ -36,10 +36,6 @@ const Login = () => {
         }
     }
 
-    /*global $, document, window, setTimeout, navigator, console, location*/
-
-
-
   return (
     <div> <br></br>
 
@@ -62,16 +58,12 @@ const Login = () => {
             {successMessage && <p className="success-message">{successMessage}</p>}
         </div> */}
 
-<a href="https://front.codes/" class="logo" target="_blank">
-		<img src="https://assets.codepen.io/1462889/fcy.png" alt=""/>
-	</a>
-
 	<div class="section">
 		<div class="container">
 			<div class="row full-height justify-content-center">
 				<div class="col-12 text-center align-self-center py-5">
 					<div class="section pb-5 pt-5 pt-sm-2 text-center">
-						<h6 class="mb-0 pb-3"><span>Log In </span><span>Sign Up</span></h6>
+						<h6 class="mb-0 pb-3"><span>Admin </span><span>User</span></h6>
 			          	<input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
 			          	<label for="reg-log"></label>
 						<div class="card-3d-wrap mx-auto">
@@ -79,7 +71,7 @@ const Login = () => {
 								<div class="card-front">
 									<div class="center-wrap">
 										<div class="section text-center">
-											<h4 class="mb-4 pb-3">Log In</h4>
+											<h4 class="mb-4 pb-3">Log In as Admin</h4>
 											<div class="form-group">
 												<input type="email" name="logemail" class="form-style" placeholder="Your Email" id="logemail" autocomplete="off"/>
 												<i class="input-icon uil uil-at"></i>
@@ -88,28 +80,28 @@ const Login = () => {
 												<input type="password" name="logpass" class="form-style" placeholder="Your Password" id="logpass" autocomplete="off"/>
 												<i class="input-icon uil uil-lock-alt"></i>
 											</div>
-											<a href="#" class="btn mt-4">submit</a>
-                            				<p class="mb-0 mt-4 text-center"><a href="#0" class="link">Forgot your password?</a></p>
+											<button className="btn mt-4">submit</button>
+											{errorMessage && <p className="error-message">{errorMessage}</p>}
+    										{successMessage && <p className="success-message">{successMessage}</p>}	
+                            				
 				      					</div>
 			      					</div>
 			      				</div>
 								<div class="card-back">
 									<div class="center-wrap">
 										<div class="section text-center">
-											<h4 class="mb-4 pb-3">Sign Up</h4>
-											<div class="form-group">
-												<input type="text" name="logname" class="form-style" placeholder="Your Full Name" id="logname" autocomplete="off"/>
-												<i class="input-icon uil uil-user"></i>
-											</div>	
+											<h4 class="mb-4 pb-3">Log In as User</h4>
 											<div class="form-group mt-2">
-												<input type="email" name="logemail" class="form-style" placeholder="Your Email" id="logemail" autocomplete="off"/>
+												<input type="email" name="logemail" value={email} onChange={(e)=>setEmail(e.target.value)} class="form-style" placeholder="Your Email" id="logemail" autocomplete="off"/>
 												<i class="input-icon uil uil-at"></i>
 											</div>	
 											<div class="form-group mt-2">
-												<input type="password" name="logpass" class="form-style" placeholder="Your Password" id="logpass" autocomplete="off"/>
+												<input type="password" value={password} name="logpass" onChange={(e)=>setPassword(e.target.value)} class="form-style" placeholder="Your Password" id="logpass" autocomplete="off"/>
 												<i class="input-icon uil uil-lock-alt"></i>
 											</div>
-											<a href="#" class="btn mt-4">submit</a>
+											<button className="btn mt-4"  onClick={handleLogin}>submit</button>
+											{errorMessage && <p className="error-message">{errorMessage}</p>}
+    										{successMessage && <p className="success-message">{successMessage}</p>}	
 				      					</div>
 			      					</div>
 			      				</div>
@@ -119,10 +111,7 @@ const Login = () => {
 		      	</div>
 	      	</div>
 	    </div>
-	</div>    
-
-
-
+	</div>    							
     </div>
   );
 }
