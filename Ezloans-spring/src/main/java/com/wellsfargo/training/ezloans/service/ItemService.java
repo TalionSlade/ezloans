48f61ee44fd1867df9da699973004babd5c5f439
TalionSlade <arpan01ghosh1307@gmail.com>
2023-09-18 17:46:06 +0530

    emp issue reg completed

--- Ezloans-spring/src/main/java/com/wellsfargo/training/ezloans/service/ItemService.java
+++ Ezloans-spring/src/main/java/com/wellsfargo/training/ezloans/service/ItemService.java
@@ -1,8 +1,11 @@
 package com.wellsfargo.training.ezloans.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.wellsfargo.training.ezloans.model.Employee;
 import com.wellsfargo.training.ezloans.model.Item;
 import com.wellsfargo.training.ezloans.repository.ItemRepository;
 
@@ -18,5 +21,8 @@ public class ItemService {
 	
 	public Item registerItems(Item i) {
 		return irepo.save(i);
+	}
+	public Optional<Item> getItem(Long id){
+		return irepo.findById(id);
 	}
 }
