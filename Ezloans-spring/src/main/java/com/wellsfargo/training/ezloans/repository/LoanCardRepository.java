70f060f4f9e98015844ed3276abec9b3f7f64515
anubhav180400 <anubhavrajput1804@gmail.com>
2023-09-22 16:08:55 +0530

    CRUD operations for loancards

--- Ezloans-spring/src/main/java/com/wellsfargo/training/ezloans/repository/LoanCardRepository.java
+++ Ezloans-spring/src/main/java/com/wellsfargo/training/ezloans/repository/LoanCardRepository.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
+import org.springframework.data.repository.query.Param;
 
 import com.wellsfargo.training.ezloans.model.LoanCard;
 
 
+
 public interface LoanCardRepository extends JpaRepository<LoanCard, Long>{
+
 
 
 }
--- Ezloans-spring/src/main/java/com/wellsfargo/training/ezloans/service/LoanCardService.java
+++ Ezloans-spring/src/main/java/com/wellsfargo/training/ezloans/service/LoanCardService.java
@@ -1,6 +1,7 @@
 package com.wellsfargo.training.ezloans.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
@@ -8,7 +9,6 @@ import org.springframework.stereotype.Service;
 
 import com.wellsfargo.training.ezloans.model.LoanCard;
 import com.wellsfargo.training.ezloans.repository.LoanCardRepository;
-
 
 import jakarta.transaction.Transactional;
 
@@ -22,6 +22,20 @@ public class LoanCardService {
 	public LoanCard addLoanCard(LoanCard l) {
 		return lrepo.save(l);
 	}
+	
+	public List<LoanCard> listAll(){
+		return lrepo.findAll(); //Invoke findAll() method of Jpa repository
+	}
+	
+	public Optional<LoanCard> getSingleLoanCard(long lid){
+		return lrepo.findById(lid);
+	}
+	
+	public void deleteLoanCard(long lid) {
+		 lrepo.deleteById(lid);
+	}
+	
+
 	
 
 }
