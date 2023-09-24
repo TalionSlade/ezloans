4a967f56f329d009b2660eb685c4f1978a8ba789
anubhav180400 <anubhavrajput1804@gmail.com>
2023-09-24 22:16:02 +0530

    get descripton added

--- Ezloans-spring/src/main/java/com/wellsfargo/training/ezloans/repository/ItemRepository.java
+++ Ezloans-spring/src/main/java/com/wellsfargo/training/ezloans/repository/ItemRepository.java
@@ -1,13 +1,18 @@
 package com.wellsfargo.training.ezloans.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.wellsfargo.training.ezloans.model.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 		
 	public Optional<Item> findByDesc(String desc);
+	
+	@Query(value = "SELECT ITEM_DESC,ITEM_VALUATION FROM ITEM_MASTER WHERE ITEM_CATEGORY=?1", nativeQuery=true)
+	public List<Object[]> getDescriptionByType(String type);
 }
 
