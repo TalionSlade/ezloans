142d4281113210585b14ba13ec3f5ea518393164
Davy Sharma <davysharma4@gmail.com>
2023-10-03 11:55:34 +0530

    Some employee controller tests

--- Ezloans-spring/src/test/java/com/wellsfargo/training/ezloans/controller/EmployeeControllerTests.java
+++ Ezloans-spring/src/test/java/com/wellsfargo/training/ezloans/controller/EmployeeControllerTests.java
@@ -25,6 +25,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.wellsfargo.training.ezloans.exception.ResourceNotFoundException;
 import com.wellsfargo.training.ezloans.model.Employee;
 import com.wellsfargo.training.ezloans.service.EmployeeService;
 
@@ -80,6 +81,98 @@ public class EmployeeControllerTests {
 		
 		verify(eservice, times(1)).registerEmployee(any(Employee.class));
 		
+	}
+	
+	@Test
+	void testCreateUser_else() throws ParseException {
+		e.setEmail("arpan@example.com");
+	    e.setFname("arpan");
+	    e.setLname("ghosh");
+	    e.setGender("male");
+	    e.setDepartment("CT");
+	    e.setDesignation("Program Associate");
+	    
+	    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+	    Date dob=new Date(df.parse("1985-01-01").getTime());
+	    e.setDob(dob);
+	    Date doj=new Date(df.parse("2023-08-23").getTime());
+	    e.setDoj(doj);
+	    e.setPassword("password");
+	    
+		//mockito methods
+		//enables stubbing methods
+		when(eservice.registerEmployee(any(Employee.class))).thenReturn(null);
+		
+		ResponseEntity<String> re=employeeController.createUser(e);
+		
+		assertEquals(HttpStatus.BAD_REQUEST, re.getStatusCode());
+		assertEquals("Registration Failed", re.getBody());
+		
+		verify(eservice, times(1)).registerEmployee(any(Employee.class));
+	}
+	
+	@Test
+	void testCreateUser_fail() throws ParseException {
+		e.setEmail("arpan@example.com");
+	    e.setFname("arpan");
+	    e.setLname("ghosh");
+	    e.setGender("male");
+	    e.setDepartment("CT");
+	    e.setDesignation("Program Associate");
+	    
+	    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+	    Date dob=new Date(df.parse("1985-01-01").getTime());
+	    e.setDob(dob);
+	    Date doj=new Date(df.parse("2023-08-23").getTime());
+	    e.setDoj(doj);
+	    e.setPassword("password");
+	    
+		//mockito methods
+		//enables stubbing methods
+	    Exception ex = new IllegalStateException();
+		when(eservice.registerEmployee(any(Employee.class))).thenThrow(ex);
+		
+		ResponseEntity<String> re=employeeController.createUser(e);
+		
+		assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, re.getStatusCode());
+		assertEquals("An Error has Occured: "+ ex.getMessage(), re.getBody());
+		
+		verify(eservice, times(1)).registerEmployee(any(Employee.class));
+	}
+	
+	@Test
+	void testLoginEmployee() throws ResourceNotFoundException {
+		e.setEmail("arpan@example.com");
+		e.setPassword("password");
+		e.setEid(1L);
+		
+		when(eservice.loginEmployee(e.getEmail())).thenReturn(Optional.of(e));
+		
+		ResponseEntity<Map<String, Object>> re = employeeController.loginEmployee(e);
+		Boolean isLoggedIn = true;
+		assertEquals(HttpStatus.OK, re.getStatusCode());
+		assertEquals(1L, re.getBody().get("eid"));
+		assertEquals(isLoggedIn, re.getBody().get("isloggedin"));
+		
+		verify(eservice, times(1)).loginEmployee(e.getEmail());
+	}
+	
+	@Test
+	void testLoginEmployee_fail() throws ResourceNotFoundException {
+		e.setEmail("arpan@example.com");
+		e.setPassword("password");
+		e.setEid(1L);
+		
+		when(eservice.loginEmployee(e.getEmail())).thenReturn(Optional.empty());
+		
+		try {
+			ResponseEntity<Map<String, Object>> re = employeeController.loginEmployee(e);
+		}
+		catch(Exception ex) {
+			assertEquals("Employee not found for this email", ex.getMessage());
+		}
+		
+		verify(eservice, times(1)).loginEmployee(e.getEmail());
 	}
 	
 }
